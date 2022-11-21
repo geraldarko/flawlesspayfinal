f767d2a27a304500e1f7a1c2041b5f93e60414d5
Paa Kwesi Addae <112646402+PaaK-A@users.noreply.github.com>
2022-11-21 08:20:57 +0000

    Update db_cred.php

--- Settings/db_cred.php
+++ Settings/db_cred.php
@@ -5,8 +5,8 @@
 // define('DB_PASSWORD', '');
 // define('DB_NAME', 'flawless_pay');
 
-define("DATABASE", "heroku_990b321786248ea");
-define("SERVER", "us-cdbr-east-06.cleardb.net");
-define("USERNAME", "b9abb5a70c70cd");
-define("PASSWD", "12f87cd8");
+define("DB_NAME", "heroku_990b321786248ea");
+define("DB_SERVER", "us-cdbr-east-06.cleardb.net");
+define("DB_USERNAME", "b9abb5a70c70cd");
+define("DB_PASSWORD", "12f87cd8");
 ?>
